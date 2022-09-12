1f0ca8756df3cc3a978a6b2185a4b3e9883aac22
enzoDamatoDev <enzodamato@outlook.com>
2022-09-12 00:33:25 -0300

    fazer esqueleto do programa

--- main.c
+++ main.c
@@ -1,32 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int transSeg(int L,int M, int S){
-    int M, S, MinS,j, vS[];
-    if(j > l){
-        imprime(vS[])
-    }
-    for (int i=0; i < L; i++){
-        L = L - 1;
-        MinS = M*60;
-        S= Mins + S;
-        vS[i] = S;
-    return vS;
-    }
+void lerAquivo(FILE* arquivo){
+	int nTestes, tamanho, nMusicas, minutos, segundos,i,j;
+	fscanf(arquivo, "%d", &nTestes);
+	for(i=0;i<nTestes;i++){
+		fscanf(arquivo, "%d", &tamanho);
+		printf("tamanho %d\n", tamanho);
+		
+		fscanf(arquivo, "%d", &nMusicas);
+		printf("num %d\n", nMusicas);
+		for(j=0;j<nMusicas;j++){
+			fscanf(arquivo, "%d", &minutos);
+			printf("minutos %d\n", minutos);
+			
+			fscanf(arquivo, "%d", &segundos);
+			printf("segundos %d\n", segundos);
+		}
+	}
 }
-int musicsFita(int n, int t, int l, int )
 
 int main()
 {
-    int n,t,l,m,s;
-    printf("Deseja gravar uma musica neh?\n");
-    printf("Quantas listas de musicas deseja gravar? \n");
-    scanf("%d",&n); //Qtd de listas de musicas.
-    printf("Tempo total para gravar as musicas na fita e quantas músicas? (Espaco para colocar os numeros)\n");
-    scanf("%d %d",&t,&l); //Grava o tempo total das musicas e a qtd de musicas
-    printf("Coloque as músicas abaixo... Min Segundo.");
-    for (int i = 0; i < l; i++)
-        scanf("%d &d", m,s);
-        transSeg(&l,&m,&s);
+	FILE *arquivo;
+	char nome[100];
+	printf("Qual o caminho do arquivo?\n");
+	scanf("%s",nome);
+	//printf("%s\n",nome);
+	arquivo = fopen(nome,"r");
+	while(arquivo == NULL){
+		printf("arquivo nao encontrado, digite o caminho completo e a extensao:\n");
+		scanf("%s",nome);
+		arquivo = fopen(nome,"r");
+	}
+	lerAquivo(arquivo);
+	
     return 0;
 }
