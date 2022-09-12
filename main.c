0959adc81aaab9d5f7b0e09edf5a3335b349f92d
EnriqueGN <enrique.novaes@gmail.com>
2022-09-11 23:01:15 -0300

    first commit

---
+++ main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int transSeg(int L,int M, int S){
+    int M, S, MinS,j, vS[];
+    if(j > l){
+        imprime(vS[])
+    }
+    for (int i=0; i < L; i++){
+        L = L - 1;
+        MinS = M*60;
+        S= Mins + S;
+        vS[i] = S;
+    return vS;
+    }
+}
+int musicsFita(int n, int t, int l, int )
+
+int main()
+{
+    int n,t,l,m,s;
+    printf("Deseja gravar uma musica neh?\n");
+    printf("Quantas listas de musicas deseja gravar? \n");
+    scanf("%d",&n); //Qtd de listas de musicas.
+    printf("Tempo total para gravar as musicas na fita e quantas músicas? (Espaco para colocar os numeros)\n");
+    scanf("%d %d",&t,&l); //Grava o tempo total das musicas e a qtd de musicas
+    printf("Coloque as músicas abaixo... Min Segundo.");
+    for (int i = 0; i < l; i++)
+        scanf("%d &d", m,s);
+        transSeg(&l,&m,&s);
+    return 0;
+}
