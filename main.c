0e316b2bab062316ad1fbb0c5fb94c4cf8e08c63
Enrique Granado Novaes <83346303+EgN03@users.noreply.github.com>
2022-09-29 16:45:55 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,20 +1,25 @@
-/* Projeto 1 - Fita Cassete 
-Integrantes: 
+/* Projeto 1 - Fita Cassete
+Integrantes:
 	Enrique Granado Novaes 32107803
-	Enzo Rocha Damato 32125992 */ 
+	Enzo Rocha Damato 32125992 */
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> //Biblioteca para chamar o malloc()
 #include <stdbool.h>
 int at=0;
 
-void imprima(int perm[],int n,int tam){
+//Função para imprimir as combinações da fita do lado A e B.
+void imprima(int perm[],int n,int tam){ //C:\Users\Rosangela\Desktop\ProjAnlise1\projFita\musicas.txt
     int i;
-    printf("Lado A\n");
+    int min, seg,min1,seg1, a;
+    printf("Lado A\n");//Imprime o lado A
     for(i = 0; i < n; i++)
-        printf("%d  \n",perm[i]);
-    printf("Lado B\n");
+        printf("%d min %d seg\n",perm[i]/60,perm[i]%60); //Converte de segundos para minutos na resolução final.
+        //printf("%d  \n",perm[i]);
+
+    printf("Lado B\n");//Imprime o lado B
     for(i = n; i < tam; i++)
-        printf("%d  \n",perm[i]);
+        //printf("%d  \n",perm[i]);
+        printf("%d min %d seg\n",perm[i]/60,perm[i]%60);//Converte de segundos para minutos na resolução final.
 }
 bool valida(int s[],int i){
 	int j;
@@ -25,6 +30,7 @@ bool valida(int s[],int i){
     return true;
 
 }
+//Função Verificação confere se a permutação atual cabe dentro da fita
 int verifica(int perm[], int nMusicas,int tempo){
 	int a=0,b=0,*fita = malloc(nMusicas * sizeof(int)),soma=0;
 	while(soma<tempo*30){
@@ -40,13 +46,14 @@ int verifica(int perm[], int nMusicas,int tempo){
 	for(b=a;b<nMusicas;b++){
 		fita[b] = perm[b];
 		soma += fita[b];
-		
+
 	}
 	if(soma>tempo*60/2)
 		return -1;
-	
+
 	return a;
 }
+//Função Permutação gera todas as permutações possiveis para as musicas
 bool permuta(int musicas[], int nMusicas, int perm[],int i,int tempo){
     if( i >= nMusicas){ // caso base
     	int a;
@@ -68,11 +75,14 @@ bool permuta(int musicas[], int nMusicas, int perm[],int i,int tempo){
     return false;
 }
 
+//Função para gravar as músicas na fita lado A e B.
 void grava(int musicas[], int tempo,int nMusicas){
-	int* perm = malloc(nMusicas * sizeof(int));
+	int* perm = malloc(nMusicas * sizeof(int)); //Aloca a um espaço x de memória
 	if(permuta(musicas,nMusicas,perm,0,tempo) == false)printf("nao ha como gravar as musicas continuamente\n");
 }
 
+//Função para ler o arquivo txt.
+//Para fica mais fácil de fazer o código, fizemos a conversão de minutos para segundos.
 void lerAquivo(FILE* arquivo){
 	int nTestes, tamanho, nMusicas, minutos, segundos,i,j,total,tMusica;
 	fscanf(arquivo, "%d", &nTestes);
@@ -80,26 +90,26 @@ void lerAquivo(FILE* arquivo){
 		total =0;
 		fscanf(arquivo, "%d", &tamanho);
 		printf("\ntamanho %d\n", tamanho);
-		
+
 		fscanf(arquivo, "%d", &nMusicas);
 		//printf("num %d\n", nMusicas);
-		
+
 		int* musicas =(int*)malloc(nMusicas * sizeof(int));
 		for(j=0;j<nMusicas;j++){
 			fscanf(arquivo, "%d", &minutos);
 			//printf("minutos %d\n", minutos);
-			
+
 			fscanf(arquivo, "%d", &segundos);
 			//printf("segundos %d\n", segundos);
 			tMusica = segundos+minutos*60;
 			total += tMusica;
 			musicas[j]=tMusica;
 		}
-		if(total > tamanho*60){
+		if(total > tamanho*60){ //Combinação das músicas ultrapassa o tempo disponível para cada lado.
 			printf("essas musicas nao cabem na fita\n");
 		}
 		else{
-			grava(musicas, tamanho, nMusicas);
+			grava(musicas, tamanho, nMusicas); //Chama a função Grava().
 		}
 	}
 }
@@ -108,16 +118,16 @@ int main()
 {
 	FILE *arquivo;
 	char nome[100]; //= "D:/mack/projAlg2/projFita-master/projFita-master/musicas.txt";
-	printf("Qual o caminho do arquivo?\n");
+	printf("Qual o caminho do arquivo?\n"); //Digite o caminho que o txt está.
 	scanf("%s",nome);
 	printf("%s\n",nome);
-	arquivo = fopen(nome,"r");
+	arquivo = fopen(nome,"r"); //Lê o arquivo txt.
 	while(arquivo == NULL){
 		printf("arquivo nao encontrado, digite o caminho completo e a extensao:\n");
 		scanf("%s",nome);
 		arquivo = fopen(nome,"r");
 	}
-	lerAquivo(arquivo);
-	
+	lerAquivo(arquivo);//Leitura do arquivo
+
     return 0;
 }
